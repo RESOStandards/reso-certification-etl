7197dc0c0a636c1df61562b2cc735724b72eaa1b
Joshua Darnell <josh@kurotek.com>
2022-06-27 18:55:47 -0700

    Issue #1: Migrated ETL code from reso-certification

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  processDataAvailability: require('./lib/process-data-availability'),
+  processMetadata: require('./lib/process-metadata'),
+  common: require('./lib/common')
+};
