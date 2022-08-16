8211679be0b526ba681e9caf90326e6c56bd20d0
Joshua Darnell <josh@kurotek.com>
2022-08-16 11:02:02 -0700

    #9: Separate Lookup Resource processor from processMetadata()

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
+const fs = require("fs/promises");
+const { Buffer } = require("node:buffer");
+const {
+  processLookupResourceMetadata,
+} = require("./lib/process-lookup-resource-metadata");
+
+const writeFile = async (path, content = "") => {
+  try {
+    await fs.appendFile(path, Buffer.from(content));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readFile = async (path = "") => {
+  try {
+    return await fs.readFile(path, { encoding: "utf8" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const transformLookupResourceMetadata = async (
+  pathToMetadataReportJson = "",
+  pathToLookupResourceData = "",
+  pathToOutputFile = ""
+) => {
+  const metadataReportJson = JSON.parse(
+      await readFile(pathToMetadataReportJson)
+    ),
+    lookupResourceJson = JSON.parse(await readFile(pathToLookupResourceData));
+
+  const results = processLookupResourceMetadata({
+    metadataReportJson,
+    lookupResourceJson,
+  });
+
+  await writeFile(pathToOutputFile, JSON.stringify(results));
+};
+
 module.exports = {
-  processDataAvailability: require('./lib/process-data-availability'),
-  processMetadata: require('./lib/process-metadata'),
-  common: require('./lib/common')
+  transformLookupResourceMetadata,
+  processDataAvailability: require("./lib/process-data-availability"),
+  processMetadata: require("./lib/process-metadata"),
+  common: require("./lib/common"),
 };
