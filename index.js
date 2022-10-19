1fbe986c5f05c1052fbf7f6d609c03203d2aee88
Mohit Srivastava <mohitsrivastava67@gmail.com>
2022-10-19 18:32:57 +0530

    #11: include the cucumber parsing method to the main export

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const { processLookupResourceMetadata, processLookupResourceMetadataFiles }  = require('./lib/process-lookup-resource-metadata');
+const {
+  processLookupResourceMetadata,
+  processLookupResourceMetadataFiles,
+} = require("./lib/process-lookup-resource-metadata");
 
 module.exports = {
   processLookupResourceMetadata,
   processLookupResourceMetadataFiles,
-  processDataAvailability: require('./lib/process-data-availability'),
-  processMetadata: require('./lib/process-metadata'),
-  common: require('./lib/common'),
+  processDataAvailability: require("./lib/process-data-availability"),
+  processMetadata: require("./lib/process-metadata"),
+  common: require("./lib/common"),
+  processCucumberJson:
+    require("./lib/process-cucumber-json").processCucumberJson(),
 };
