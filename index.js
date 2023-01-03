890441aa01d49b9a9a56d11fde8576706b7683a1
mohit-s96 <mohitsrivastava67@gmail.com>
2023-01-03 13:20:36 +0530

    #4: update imports for PAT/MAT reports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
   processDataAvailability: require('./lib/process-data-availability'),
   processMetadata: require('./lib/process-metadata'),
-  common: require('./lib/common')
+  common: require('./lib/common'),
 };
