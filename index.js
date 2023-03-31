06780b9b3976b0b695e0def8eeaef4d767a8c10d
darnjo <josh@reso.org>
2023-03-31 16:07:27 -0700

    #23: Added export for metadata references

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const {
   processLookupResourceMetadataFiles,
 } = require('./lib/process-lookup-resource-metadata');
 
+const ddMetadata_1_7 = require('./lib/references/dd-1.7/metadata-report.json');
+
 module.exports = {
   processLookupResourceMetadata,
   processLookupResourceMetadataFiles,
   processDataAvailability: require('./lib/process-data-availability'),
   processMetadata: require('./lib/process-metadata'),
   common: require('./lib/common'),
-  processCucumberJson: require('./lib/process-cucumber-json')
+  processCucumberJson: require('./lib/process-cucumber-json'),
+  ddMetadata_1_7
 };
