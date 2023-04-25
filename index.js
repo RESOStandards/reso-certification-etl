080e450bf4ba7aeb5251c1c046e9380f576aabe5
Joshua Darnell <darnjo@users.noreply.github.com>
2023-04-25 08:27:32 -0700

    #23 Expose metadata references in lib (#24)
    * #23: Added export for metadata references
    
    * #23: Add accessor function for versioned metadata
    
    * #23: Add improved error message for nonexistent versions
    
    * Create metadata-report.json
    
    * Update metadata-report.json
    
    * Update metadata-report.json
    
    * Delete metadata-report.json
    
    * Create metadata-report.json
    
    * #23: Updated metadata reference files
    
    ---------
    
    Co-authored-by: darnjo <josh@reso.org>

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const {
   processLookupResourceMetadataFiles,
 } = require('./lib/process-lookup-resource-metadata');
 
+const getReferenceMetadata = (version = '1.7') => {
+  try {
+    return require(`./lib/references/dd-${version}/metadata-report.json`);
+  } catch (err) {
+    console.error(`Cannot load reference metadata for version '${version}'!`);
+    return null;
+  }
+};
+
 module.exports = {
   processLookupResourceMetadata,
   processLookupResourceMetadataFiles,
   processDataAvailability: require('./lib/process-data-availability'),
   processMetadata: require('./lib/process-metadata'),
   common: require('./lib/common'),
-  processCucumberJson: require('./lib/process-cucumber-json')
+  processCucumberJson: require('./lib/process-cucumber-json'),
+  getReferenceMetadata
 };
