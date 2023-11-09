99c42461eb4ecd1c7e82d908029c36e4eda0e6df
Mohit Srivastava <mohitsrivastava67@gmail.com>
2023-11-09 08:33:24 +0530

    #19: add relevant mocha tests from cert repo (#30)
    * #11: typo fix
    
    * #19: rename cucumber test and reference reports
    
    * #19: added tests for processDD and processDA methods

---
+++ test/data-dictionary.js
@@ -0,0 +1,13 @@
+const { metadataReport } = require('./sample-data-dictionary-reports/metadata-report');
+const expectedReport = require('./sample-data-dictionary-reports/expected-metadata-report.json');
+const {
+  processMetadata: { processMetadataReport }
+} = require('..');
+const assert = require('assert');
+
+describe('process-metadata', function () {
+  it('should match the expected data', () => {
+    const processedReport = processMetadataReport(metadataReport);
+    assert.deepStrictEqual(JSON.parse(JSON.stringify(processedReport)), expectedReport);
+  });
+});
