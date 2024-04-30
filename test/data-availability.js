b9d48aebb1f8f35a070925633b171a46333e02eb
mohit-s96 <mohitsrivastava67@gmail.com>
2024-05-01 00:26:14 +0530

    #62: add new failing tests

--- test/data-availability.js
+++ test/data-availability.js
@@ -1,5 +1,6 @@
-const { daReport } = require('./sample-data-availability-reports/availability-report');
+const { daReport, daReport2_0 } = require('./sample-data-availability-reports/availability-report');
 const expectedReport = require('./sample-data-availability-reports/expected-availability-report.json');
+const expectedReport2_0 = require('./sample-data-availability-reports/expected-availability-report-2_0.json');
 const {
   processDataAvailability: { processDataAvailability }
 } = require('..');
@@ -9,5 +10,10 @@ describe('processDataAvailability', function () {
   it('should match the expected data', async () => {
     const processedReport = await processDataAvailability(daReport);
     assert.deepStrictEqual(JSON.parse(JSON.stringify(processedReport)), expectedReport);
+  });
+
+  it('should match the expected data with new resources in DD 2.0', async () => {
+    const processedReport = await processDataAvailability(daReport2_0);
+    assert.deepStrictEqual(JSON.parse(JSON.stringify(processedReport)), expectedReport2_0);
   });
 });
